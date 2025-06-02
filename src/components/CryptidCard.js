c6404a31327ca6a2878ebd6d867031669198a43c
Ben Johnson <greypilgrim115@gmail.com>
2025-06-02 17:05:49 -0500

    filter tags functional, added checkbox for aggressive cryptids

--- src/components/CryptidCard.js
+++ src/components/CryptidCard.js
@@ -27,6 +27,15 @@ export default function CryptidCard({ cryptObj, onUpdate }) {
           Sightings
         </Card.Link>
         <br />
+        {cryptObj.aggressive === true && (
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '2%' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z" />
+            </svg>
+            <p style={{ color: 'black', fontSize: '15px', marginLeft: '6px', marginBottom: 0 }}>Aggressive</p>
+          </div>
+        )}
+
         {user.uid === cryptObj.uid && (
           <Link href={`/Cryptids/edit/${cryptObj.firebaseKey}`}>
             <Button className="cardBtn" style={{ marginTop: '20px' }}>
@@ -51,6 +60,7 @@ CryptidCard.propTypes = {
     image: PropTypes.string.isRequired,
     firebaseKey: PropTypes.string.isRequired,
     uid: PropTypes.string,
+    aggressive: PropTypes.bool,
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
 };
